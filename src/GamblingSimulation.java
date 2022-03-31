0dee5238b893bfae38af83f5a8c44d70e5a7728f
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-31 12:47:41 +0530

    add UC2

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,24 +1,45 @@
 
 public class GamblingSimulation
 {
-private	int Start_Every_Day=100;
-private int Bet_Every_Game=1;
+   public static int Stake=100;
+   public static int Bet=1;
+   public static int stake;
+   public static int Win=0;
+   public static int Loose=0;
 	  public static void WinLoss() 
 	 { 
-		 
-		  int win=0;
-		  int loos=0;
-		   if(Math.random()<0.5) {
-			win++;
-			System.out.println("win the game " +win);
-		   }
-		   else   {
-			 loos--;
-			System.out.println("loss the game"+loos);
-		}
-	}
+		  double winOrLoss = Math.floor(Math.random() * 10) % 2;
+          if (Bet == winOrLoss) {
+              System.out.println("Gambler has Won");
+              Stake = Stake +1;
+              System.out.println("The current stake is " + Stake);
+          }
+          else
+          {
+              System.out.println("The Gambler has lost");
+              Stake = Stake -1;
+              System.out.println("The remaining stake is: " + Stake);
+          }
+	 }
+	  
+	  public static int resign(){
+	        while(Stake > 50 && Stake< 150) {
+	        	WinLoss();
+	        }
+	        if(Stake == 50) {
+	            System.out.println("The Gambler LOST 50% of stake, So he have to resign for the day. ");
+	            
+	        }
+	        else {
+	            System.out.println("The Gambler WON 50% of stake, So he have to resign for the day. ");
+	        }
+	        System.out.println(" ");
+	        return 0;
+	    }
+	 
 	public static void main(String args [])
 	{
 		WinLoss();
+		resign();
 	}
 }
