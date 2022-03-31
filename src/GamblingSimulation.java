fb69cd6d7f249089ecf0f5c659361b773a35680c
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-31 09:17:00 +0530

    add UC2

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,18 +1,19 @@
 
 public class GamblingSimulation
 {
-	int Start_Every_Day=100;
-	int Bet_Every_Game=1;
-	public static void WinLoss() 
-	{   int cash =1;
-		int win=0;
-		int loos=0;
-		if(Math.random()<0.5) {
+private	int Start_Every_Day=100;
+private int Bet_Every_Game=1;
+	  public static void WinLoss() 
+	 { 
+		 
+		  int win=0;
+		  int loos=0;
+		   if(Math.random()<0.5) {
 			win++;
 			System.out.println("win the game " +win);
-		}
-		else {
-			loos--;
+		   }
+		   else   {
+			 loos--;
 			System.out.println("loss the game"+loos);
 		}
 	}
