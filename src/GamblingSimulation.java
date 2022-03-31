6f6ade562dee61697c6e9b56dbd85c6734b73589
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-31 08:45:18 +0530

    add UC1

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,4 +1,10 @@
 
-public class GamblingSimulation {
-
+public class GamblingSimulation
+{
+	int Start_Every_Day=100;
+	int Bet_Every_Game=1;
+	public static void main(String args [])
+	{
+		
+	}
 }
